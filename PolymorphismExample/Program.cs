dd19903a60d04a0cd49457434b7c903d83d1f1b4
bi24vr <bi24vr@FOT-CELLA15>
2024-10-24 09:59:10 +0100

    Add project files.

---
+++ PolymorphismExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymorphismExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
