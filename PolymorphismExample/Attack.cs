1bb013dc73ce776e03af7aba0946282e08f7e48d
1SaiVaruna <sai030804@gmail.com>
2024-10-24 10:10:02 +0100

    Created internal classes DosAttack and MalwareAttack.

--- PolymorphismExample/Attack.cs
+++ PolymorphismExample/Attack.cs
@@ -8,6 +8,39 @@ namespace PolymorphismExample
 {
     internal class Attack
     {
-        public.
+        public string Name { get; set; }
+
+        public virtual void DetectAttack()
+        {
+            Console.WriteLine("Detecting attack...");
+        }
+    }
+
+    internal class DoSAttack : Attack
+    {
+        public DoSAttack()
+        {
+            Name = "Denial-of-Service Attack";
+        }
+
+        public override void DetectAttack()
+        {
+            Console.WriteLine($"Detecting {Name}: Looking for excessive network traffic...");
+
+        }
+
+        internal class MalwareAttack : Attack
+        {
+            public MalwareAttack()
+            {
+                Name = "Malware Infection";
+            }
+
+            public override void DetectAttack()
+            {
+                Console.WriteLine($"Detecting {Name}: Analyzing system logs for suspicious activitiy...");
+            }
+        }
+
     }
 }
